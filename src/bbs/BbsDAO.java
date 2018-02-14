3373642f5ba884a9e927b728716b37df5c9b58e3
choimyungsu <36438698+choimyungsu@users.noreply.github.com>
2018-02-14 23:00:16 +0900

    Add files via upload

--- src/bbs/BbsDAO.java
+++ src/bbs/BbsDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class BbsDAO {
 	
@@ -66,5 +67,91 @@ public class BbsDAO {
 			e.printStackTrace();
 		}
 		return -1;//데이터베이스 오류
-	}	
+	}
+	public ArrayList<Bbs> getList(int pageNumber){
+		
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		ArrayList<Bbs> list = new ArrayList<Bbs>();
+		
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber -1) * 10);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				list.add(bbs);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;//
+	}
+	//페이징 처리를 위한 함수
+	public boolean nextPage(int pageNumber) {
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			int aaa = getNext() - (pageNumber -1) * 10;
+			System.out.println("nextPage: 인자값은" + aaa);
+			pstmt.setInt(1, aaa);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return false;//
+	}
+	
+	public Bbs getBbs(int bbsID) {
+		
+		String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, bbsID);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				return bbs;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;//
+	}
+	
+	public int update(int bbsID, String bbsTitle, String bbsContents) {
+		
+		String SQL = "UPDATE BBS SET bbsTitle = ? , bbsContent = ? WHERE bbsID = ?";
+				
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			
+			pstmt.setString(1, bbsTitle);
+			pstmt.setString(2, bbsContents);
+			pstmt.setInt(3, bbsID);
+			
+			return pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1;//데이터베이스 오류
+		
+	}
+	
 }
